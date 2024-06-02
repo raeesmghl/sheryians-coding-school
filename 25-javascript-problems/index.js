db0941b480e50d87028c36c1d9f3162b31e968c8
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-06-02 19:16:15 +0500

    Update index.js

--- 25-javascript-problems/index.js
+++ 25-javascript-problems/index.js
@@ -1,1 +1,16 @@
 
+// // problem 1 : reverse each word in a string;
+
+// let str = 'pakistan is my country';
+
+// // sheryians solution
+// let mp = str.split(' ').map((word)=>{
+//     return word.split('').reverse().join('');
+// })
+
+// console.log(mp.join(' '))
+
+
+// // my solution
+
+// let arr = str.split('').reverse().join('').split(' ').reverse().join(' ');
