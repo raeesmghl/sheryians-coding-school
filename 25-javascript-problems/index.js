a589fcfd7ac90cdbae6ba427a9ce48ddac771350
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-06-02 19:19:02 +0500

    Update index.js

--- 25-javascript-problems/index.js
+++ 25-javascript-problems/index.js
@@ -23,3 +23,55 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // comment reply
+
+// let arr = [2, 3, 4, 2, 3, 2, 5, 5, 6, 6, 7, 8, 8,8,8,5,8,8, 7,8,5,];
+// let fincMostFrequent = (arr)=>{
+//     let frequency = {};
+//     let mostFrequent = 0;
+
+//     for(let num of arr){
+//         frequency[num] = (frequency[num] || 0) +1;
+
+//         if(frequency[num]>mostFrequent){
+//             mostFrequent = num;
+//         }
+//     }
+//     return mostFrequent;
+// }
+
+// console.log(fincMostFrequent(arr))
+
+
+
