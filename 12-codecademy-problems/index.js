31346c6ede43cde47fba1a3adc0762cecdd941a6
Raees Mughal <raees.mughal.official@gmail.com>
2024-06-30 21:57:12 +0500

    Update index.js

--- 12-codecademy-problems/index.js
+++ 12-codecademy-problems/index.js
@@ -1,1 +1,14 @@
 
+
+
+// solution 1: 
+
+function func (n) {
+    let arr = [];
+    for(let i = 1;i<=n;i++){
+        if(i%2==0){
+            arr.push(i);
+        }
+    }
+    return arr.join(' ')
+}
