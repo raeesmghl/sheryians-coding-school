c5724badd6e68a13cc3d741d2aeaae5093d77414
Raees Mughal <raees.mughal.official@gmail.com>
2024-07-01 04:27:03 +0500

    Update solutions.js

--- 12-codecademy-problems/solutions.js
+++ 12-codecademy-problems/solutions.js
@@ -221,4 +221,5 @@ let ab = [1,2,8,2,5,7,1,3,1,3,5,7,0];
 
 console.log(Math.max.apply(null,ab));
 
+// if you want to find the most frequent el in array, check 'comment reply example' in '25 javascript problems' by sheryians coding school
 
